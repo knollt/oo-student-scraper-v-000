660c98997cf4c4c98bb9b1e25eb2821562bfe827
Tim Knoll <timothy.knoll@gmail.com>
2019-04-11 02:15:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,9 +8,8 @@ attr_accessor :students
 
   def self.scrape_index_page(index_url)
     index_doc = Nokogiri::HTML(open(index_url)
-    student_cards = index_doc.css(".student-card")
     students = []
-    student_cards.each do |student_card|
+    student_cards = index_doc.css(".student-card").each do |student_card|
       students << {
         :name => student_card.css("h4.student-name").text,
         :location => student_card.css("p.student-location").text,
